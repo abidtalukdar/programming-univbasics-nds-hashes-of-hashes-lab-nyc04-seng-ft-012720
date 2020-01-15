d5c12d9590a556feb7fe86d56be02a4a4b7f9380
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-15 05:28:07 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,7 +54,9 @@ def naming_system
             sub_category: {
               label: "Genus",
               sub_category: {
-                label: "Species"
+                label: "Species",
+                sub_category: {
+                }
               }
             }
           }
